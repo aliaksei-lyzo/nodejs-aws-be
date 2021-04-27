77fc7ceb515424d28ef7cb38e8ddea6e879787ab
Aliaksei Lyzo <lyzo.alex@gmail.com>
2021-04-27 13:44:59 +0300

    Task5 - importFileParser

--- import-service/handler.js
+++ import-service/handler.js
@@ -1,14 +1,14 @@
-const AWS = require('aws-sdk');
-
 module.exports = {
   importProductsFile: async function(event) {
     console.log('EVENT', event);
     try {
+      const AWS = require('aws-sdk');
+      const BUCKET = 'speedworks-products';
+
       const filename = event.queryStringParameters.name;
       console.log('filename', filename)
       if (!filename) throw new Error('Missing required parameter');
       const s3 = new AWS.S3({ region: 'eu-west-1' });
-      const BUCKET = 'speedworks-products';
       const catalogPath = `uploaded/${filename}`;
       
       const params = {
@@ -37,6 +37,60 @@ module.exports = {
         })
       }
     }
+  },
+
+  importFileParser: async function(event) {
+    console.log('EVENT', JSON.stringify(event));
+    try {
+      const AWS = require('aws-sdk');
+      const csv = require('csv-parser');
+      const BUCKET = 'speedworks-products';
+
+      const s3 = new AWS.S3({ region: 'eu-west-1' });
+      const record = event.Records[0];
+
+      const products = [];
+      console.log(`record`, record);
+      const csvStream = s3.getObject({ Bucket: BUCKET, Key: record.s3.object.key })
+      .createReadStream();
+      console.log('csvStream', JSON.stringify(csvStream));
+      await new Promise((res) => {
+        csvStream
+        .on('error', (err) => {
+          console.log('Error in stream');
+          console.log(JSON.stringify(err));
+         })
+        .pipe(csv())
+        .on('data', product => {
+          products.push(product);
+        })
+        .on('end', async () => {
+          console.log('Parsed successfully');
+          console.log(products);
+          res('okay');
+        })
+      });
+
+      console.log('Moving file to parsed');
+      await s3.copyObject({
+        Bucket: BUCKET,
+        CopySource: `/${BUCKET}/${record.s3.object.key}`,
+        Key: record.s3.object.key.replace('uploaded', 'parsed')
+      }).promise();
+
+      console.log('Deleting file from uploaded');
+      await s3.deleteObject({
+        Bucket: BUCKET,
+        Key: record.s3.object.key,
+      }).promise();
+
+      console.log('All done!');
+      return 200
+    } catch (e) {
+      console.log('ERROR', JSON.stringify(e, Object.getOwnPropertyNames(e)));
+      return 500;
+    }
+    
   }
 
 }
